d1c14ca420142fb80b5152ecf709854f2a5f3cc8
Daniel Mori <danielstelzer@gmail.com>
2024-09-11 10:27:56 -0300

    Initial Commit

---
+++ FlickrAppGarden/FlickrAppGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrAppGardenApp.swift
+//  FlickrAppGarden
+//
+//  Created by Daniel Mori on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrAppGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
